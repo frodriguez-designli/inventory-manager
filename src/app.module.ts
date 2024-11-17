5bba5deb392c055c7f5cc4edeb5118b9fd08ad5a
Engineer_Franyeliza <franyelizarodriguez@live.com>
2024-11-17 12:53:27 -0400

    feat: added application modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CustomerModule } from './customer/customer.module';
+import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [],
+  imports: [CustomerModule, ProductModule, OrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
