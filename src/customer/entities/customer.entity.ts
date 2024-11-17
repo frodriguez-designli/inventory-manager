bb486281575f9d8e8fd08142ed16208ab66d9f8f
Engineer_Franyeliza <franyelizarodriguez@live.com>
2024-11-17 13:12:23 -0400

    feat: added customer entity and dto

--- src/customer/entities/customer.entity.ts
+++ src/customer/entities/customer.entity.ts
@@ -1,1 +1,20 @@
-export class Customer {}
+import { Exclude } from 'class-transformer';
+
+export class Customer {
+  customer_id: number;
+
+  customer_name: string;
+
+  email: string;
+
+  phone?: string;
+
+  @Exclude() 
+  password: string;
+
+  createdAt: Date;
+
+  updatedAt: Date;
+
+  deletedAt?: Date;
+}
