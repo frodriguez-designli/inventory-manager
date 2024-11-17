084db07e445b27ac4e05356e7ad4654d92ad16e5
Engineer_Franyeliza <franyelizarodriguez@live.com>
2024-11-17 12:57:43 -0400

    feat: added missing modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { AppService } from './app.service';
 import { CustomerModule } from './customer/customer.module';
 import { ProductModule } from './product/product.module';
 import { OrderModule } from './order/order.module';
+import { CategoryModule } from './category/category.module';
+import { PaymentTypeModule } from './payment-type/payment-type.module';
+import { OrderTypeModule } from './order-type/order-type.module';
+import { OrderProductModule } from './order-product/order-product.module';
 
 @Module({
-  imports: [CustomerModule, ProductModule, OrderModule],
+  imports: [CustomerModule, ProductModule, OrderModule, CategoryModule, PaymentTypeModule, OrderTypeModule, OrderProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
